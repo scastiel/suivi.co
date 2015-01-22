0514aa3dc9a816ea6f1b6d843a6a6264b9c88395
Sébastien Castiel <sebastien.castiel@gmail.com>
2015-01-22 20:40:12 +0100

    Disabling local analytics

--- client/scripts/analytics.js
+++ client/scripts/analytics.js
@@ -8,7 +8,7 @@ module.exports = {
 			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-		ga('create', analyticsId, { 'cookieDomain': 'none' });
+		ga('create', analyticsId, 'auto');
 		ga('send', 'pageview');
 
 		return ga;
