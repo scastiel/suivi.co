91c3024ad903eab921d4a668523335e4b6d21bc2
Sébastien Castiel <sebastien.castiel@gmail.com>
2015-01-23 22:15:14 +0100

    Adding content : well, footer...

--- client/scripts/components/login-form.jsx
+++ client/scripts/components/login-form.jsx
@@ -6,12 +6,23 @@ var LoginForm = React.createClass({
 
 	render: function LoginForm_render() {
 		return (
-			<div className="row">
-				<div className="col-sm-6">
-					<SigninForm appComponent={this.props.appComponent} loginPostUri={this.props.loginPostUri}/>
+			<div>
+				<div className="row">
+					<div className="col-sm-12">
+						<div className="well well-lg">
+							<p>Vous achetez sur Internet et recevez régulièrement des colis ?</p>
+							<p>Mais savez-vous à chaque fois comment suivre ces colis ?</p>
+							<p>Et si un site vous permettait de suivre n'importe quel colis, <strong>quelque soit le transporteur</strong> ?</p>
+						</div>
+					</div>
 				</div>
-				<div className="col-sm-6">
-					<SignupForm appComponent={this.props.appComponent} signupPostUri={this.props.signupPostUri}/>
+				<div className="row">
+					<div className="col-sm-6">
+						<SigninForm appComponent={this.props.appComponent} loginPostUri={this.props.loginPostUri}/>
+					</div>
+					<div className="col-sm-6">
+						<SignupForm appComponent={this.props.appComponent} signupPostUri={this.props.signupPostUri}/>
+					</div>
 				</div>
 			</div>
 		);
