785edd30c0e4c9541924d9cd29dc0f33de9e035e
Sébastien Castiel <sebastien.castiel@gmail.com>
2015-02-01 17:58:58 +0100

    Design improvements

--- client/scripts/components/tracking-number-form.jsx
+++ client/scripts/components/tracking-number-form.jsx
@@ -65,15 +65,18 @@ var TrackingNumberForm = React.createClass({
 			<form role="form" onSubmit={this.onOkButtonClick}>
 				<div className="form-group">
 					<label htmlFor="txtTrackingNumber">Votre numéro de colis&nbsp;:</label>
-					<input type="text" className="form-control input-lg" name="trackingNumber" id="txtTrackingNumber"
-						placeholder="12345-67890-A" onChange={this.handleTrackingNumberChange}
-						value={this.state.trackingNumber}
-						autoComplete="off" autoCorrect="off" autoCapitalize="off" spellCheck="false"/>
+					
+					<div className="input-group">
+						<input type="text" className="form-control input-lg" name="trackingNumber" id="txtTrackingNumber"
+							placeholder="12345-67890-A" onChange={this.handleTrackingNumberChange}
+							value={this.state.trackingNumber}
+							autoComplete="off" autoCorrect="off" autoCapitalize="off" spellCheck="false"/>
+						<span className="input-group-btn">
+					    	<button className="btn btn-default btn-lg" type="submit"
+					    		disabled={this.state.trackingNumber !== '' ? '' : 'disabled'}>Valider</button>
+					    </span>
+					</div>
 				</div>
-				<button type="submit" className="btn btn-default btn-lg"
-					disabled={this.state.trackingNumber !== '' ? '' : 'disabled'}>
-					Valider
-				</button>
 			</form>
 		);
 	}
