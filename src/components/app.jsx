45958f364205ad1eb47157c4dee46382006495e8
Sébastien Castiel <sebastien.castiel@gmail.com>
2015-02-09 10:49:55 +0100

    Document title

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -6,17 +6,20 @@ var Footer = require('./footer.jsx');
 var React = require('react');
 var Router = require('react-router');
 var RouteHandler = Router.RouteHandler;
+var DocumentTitle = require('react-document-title');
 
 var App = React.createClass({
 	render: function() {
 		return (
-			<div>
-				<Navbar/>
-				<div className="main">
-					<RouteHandler/>
+			<DocumentTitle title="Suivez vos colis sur un site unique – Suivi.co">
+				<div>
+					<Navbar/>
+					<div className="main">
+						<RouteHandler/>
+					</div>
+					<Footer/>
 				</div>
-				<Footer/>
-			</div>
+			</DocumentTitle>
 		);
 	}
 });
