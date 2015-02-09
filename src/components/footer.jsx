3c8a1fa3d5226ccec17bf9fa2c838143e34ea08f
Sébastien Castiel <sebastien.castiel@gmail.com>
2015-02-09 22:35:47 +0100

    Removing Addthis

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -7,8 +7,8 @@ var Footer = React.createClass({
 			<footer className="footer">
 				<div className="container">
 					<p className="text-muted">
-						&copy; <a href="https://twitter.com/scastiel">Sébastien Castiel</a> 2015 – Une remarque&nbsp;? <a href="mailto:sebastien@suivi.co">sebastien@suivi.co</a> – Parler de nous&nbsp;: 
-						<span className="addthis_sharing_toolbox"></span>
+						&copy; Sébastien Castiel 2015 – Une remarque&nbsp;? <a href="mailto:sebastien@suivi.co">sebastien@suivi.co</a> –
+						Suivez-nous sur&nbsp;<a href="http://twitter.com/suivi_co" target="_blank" title="Suivi.co sur Twitter">Twitter</a>.
 					</p>
 				</div>
 		    </footer>
