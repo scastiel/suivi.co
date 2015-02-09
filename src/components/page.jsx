b702a6e0673aa82483fa33207370e5a66ab5e177
Sébastien Castiel <sebastien.castiel@gmail.com>
2015-02-09 10:49:55 +0100

    Document title

--- src/components/page.jsx
+++ src/components/page.jsx
@@ -1,19 +1,22 @@
 
 var React = require('react');
+var DocumentTitle = require('react-document-title');
 
 var Page = React.createClass({
 	render: function() {
 		return (
-			<div className="page">
-				<div className="panel">
-					<div className="panel-body">
-						<div className="page-header">
-							<h2>{this.props.title}</h2>
+			<DocumentTitle title={this.props.title + " – Suivez vos colis sur un site unique – Suivi.co"}>
+				<div className="page">
+					<div className="panel">
+						<div className="panel-body">
+							<div className="page-header">
+								<h2>{this.props.title}</h2>
+							</div>
+							{this.props.children}
 						</div>
-						{this.props.children}
 					</div>
 				</div>
-			</div>
+			</DocumentTitle>
 		);
 	}
 });
--- src/routes/site.jsx
+++ src/routes/site.jsx
@@ -1,6 +1,7 @@
 
 var React = require('react');
 var Router = require('react-router');
+var DocumentTitle = require('react-document-title');
 
 var routes = require('./react-routes.jsx');
 
@@ -8,9 +9,11 @@ module.exports = function(app) {
 	return function (req, res) {
 		Router.run(routes, req.url, function (Handler, other) {
 			var content = React.renderToString(<Handler/>);
+			var title = DocumentTitle.rewind();
 			if (content.indexOf("<!--404-->") !== -1)
 				res.status(404);
 			res.render('index', {
+				title: title,
 				content: content,
 				config: require('../config')
 			});
