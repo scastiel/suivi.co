db50bfd525912c2c89658e57534a1a48dee81b4b
Sébastien Castiel <sebastien.castiel@gmail.com>
2015-02-11 16:51:43 +0100

    Reorganizing soyrce files

--- src/client/main.jsx
+++ src/client/main.jsx
@@ -5,7 +5,7 @@ require('bootstrap');
 var React = require('react');
 var Router = require('react-router');
 
-var routes = require('../routes/react-routes.jsx');
+var routes = require('../lib/react-routes.jsx');
 
 Router.run(routes, Router.HistoryLocation, function (Handler) {
 	React.render(<Handler/>, document.getElementById('app'), function() {
