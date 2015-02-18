1a78efb6562294ecda443a07ccc5f3c98a631e0b
Sébastien Castiel <sebastien.castiel@gmail.com>
2015-02-18 16:47:09 +0100

    Adding USPS carrier

--- src/lib/components/carriers-page.jsx
+++ src/lib/components/carriers-page.jsx
@@ -14,6 +14,7 @@ var CarriersPage = React.createClass({
 					<li><img src="/assets/logo-chronopost.png" alt="Chronopost"/></li>
 					<li><img src="/assets/logo-ups.png" alt="UPS"/></li>
 					<li><img src="/assets/logo-postnl.png" alt="PostNL"/></li>
+					<li><img src="/assets/logo-usps.png" alt="USPS"/></li>
 				</ul>
 
 				<p><strong>Suivi.co</strong> est actuellement en phase de développement, c'est pourquoi tous les transporteurs ne sont pas
